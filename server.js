8182f4449161de98c71a7ed2304fbee75f9dac6f
Chris Wen <35661593+wenyupeng@users.noreply.github.com>
2025-05-14 17:37:33 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json);
+
+
+app.listen(port, ()=>{
+    console.log(`Server: http://localhost:${port}`);
+});
